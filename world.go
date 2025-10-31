99b6e88be2ca6952754ba33851ead3524a0a8366
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-31 21:17:34 +0530

    Updated message order on house enter

--- world.go
+++ world.go
@@ -13,10 +13,10 @@ func setupWorld() {
 			Items:	[]string{},
 		},		
 		"Hallway": {
-			Name:        "Hallway",
-			Desc: "Old portraits stare at you from the walls. There’s a locked door to the east.",
-			Exits:       map[string]string{"south": "Foyer", "east": "Library", "west": "Kitchen"},
-			Items:       []string{"silver key"},
+			Name:	"Hallway",
+			Desc:	"Old portraits stare at you from the walls. There’s a locked door to the east.",
+			Exits:	map[string]string{"south": "Foyer", "east": "Library", "west": "Kitchen"},
+			Items:	[]string{"silver key"},
 		},
 	}
 
