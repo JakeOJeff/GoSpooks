a6a080a3ed2f4ccb45cd32d222ab993d8cf14607
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-02 17:31:22 +0530

    Added 3 Rooms

--- world.go
+++ world.go
@@ -1,28 +1,51 @@
 package main
+
 import (
-    "fmt"
-	"strings"
+	"fmt"
 	"math/rand"
+	"strings"
 )
+
 func setupWorld() {
-	rooms = map[string]*Room {
+	rooms = map[string]*Room{
 		"Foyer": {
-			Name:	"Foyer",
-			Desc:	"A dimly lit entrance. A cold breeze pushes past you.",
-			Exits:	map[string]string{"north": "Hallway"},
-			Items:	[]string{"blue key"},
-		},		
+			Name:  "Foyer",
+			Desc:  "A dimly lit entrance. A cold breeze pushes past you.",
+			Exits: map[string]string{"north": "Hallway"},
+			Items: []string{"blue key"},
+		},
 		"Hallway": {
-			Name:	"Hallway",
-			Desc:	"Old portraits stare at you from the walls. There’s a locked door to the east.",
-			Exits:	map[string]string{"south": "Foyer", "east": "Library", "west": "Kitchen"},
-			Items:	[]string{"silver key"},
+			Name:  "Hallway",
+			Desc:  "Old portraits stare at you from the walls. There’s a locked door to the east.",
+			Exits: map[string]string{"south": "Foyer", "east": "Library", "west": "Kitchen"},
+			Items: []string{"silver key"},
+		},
+		"Kitchen": {
+			Name:  "Kitchen",
+			Desc:  "Pots and pans stay solemn as the doors waver",
+			Exits: map[string]string{"east": "Hallway"},
+			Items: []string{"Sledge Hammer"},
+		},
+		"Library": {
+			Name:   "Library",
+			Desc:   "Dusty books cover the shelves as they point as if watching you every move.",
+			Exits:  map[string]string{"west": "Hallway", "north": "Secret Vault"},
+			Locked: true,
+			Key:    "silver key",
+		},
+		"Secret Vault": {
+			Name:   "Secret Vault",
+			Desc:   "This room is so dark and empty, you feel around for doors.",
+			Exits:  map[string]string{"down": "Dark Basement", "south": "Library"},
+			Locked: true,
+			Key:    "Sledge Hammer",
 		},
 	}
 
 	player = Player{CurrentRoom: "Foyer"}
 	events = []Event{
 		{"Someones here", 20},
+		{"You hear a whisper, as it creeps near you", 40},
 	}
 }
 
